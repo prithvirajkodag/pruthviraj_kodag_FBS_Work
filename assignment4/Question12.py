e7179e862c11307a74e4b298c210d4dd03eac80b
prithvirajkodag <prithvirajkodag@gmail.com>
2026-07-23 08:41:54 +0530

    Add files via upload

---
+++ assignment4/Question12.py
@@ -0,0 +1,20 @@
+##WAP to check if a given number is Strong Number
+n = int(input("Enter a number: "))
+
+temp = n
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    fact = 1
+
+    for i in range(1, digit + 1):
+        fact *= i
+
+    sum += fact
+    temp //= 10
+
+if sum == n:
+    print("Strong Number")
+else:
+    print("Not Strong Number")
---
+++ assignment4/Question7.py
@@ -0,0 +1,13 @@
+##WAP to check if a given number is prime or not
+n = int(input("Enter a number: "))
+
+count = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+
+if count == 2:
+    print("Prime Number")
+else:
+    print("Not Prime Number")
